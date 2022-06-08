66520833c63621e4174b7b9ceeb7dd1e137ce48b
Mostafa <mosi3883@gmail.com>
2022-06-09 03:51:36 +0430

    sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,38 @@
 import React from 'react';
 import { FaTimes } from 'react-icons/fa';
+import { useGlobalContext } from './context';
 import sublinks from './data';
 
 const Sidebar = () => {
-  return <h2>sidebar component</h2>;
+  const { isSidebarOpen, closeSidebar } = useGlobalContext();
+  return (
+    <aside className={`sidebar-wrapper${isSidebarOpen ? ' show' : ''}`}>
+      <div className='sidebar'>
+        <button className='close-btn' onClick={closeSidebar}>
+          <FaTimes />
+        </button>
+        <div className='sidebar-links'>
+          {sublinks.map((item, index) => {
+            return (
+              <article key={index}>
+                <h4>{item.page}</h4>
+                <div className='sidebar-sublinks'>
+                  {item.links.map((link, index) => {
+                    return (
+                      <a href={link.url} key={index}>
+                        {link.icon}
+                        {link.label}
+                      </a>
+                    );
+                  })}
+                </div>
+              </article>
+            );
+          })}
+        </div>
+      </div>
+    </aside>
+  );
 };
 
 export default Sidebar;
